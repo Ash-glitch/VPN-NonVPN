69bef74632e29f9fa5cb7e99d3278b781b262d4f
Ash-glitch <54162854+Ash-glitch@users.noreply.github.com>
2024-10-07 06:13:47 +0530

    Update Scenario_A2_NON-VPN.py

--- Scenario_A2_NON-VPN.py
+++ Scenario_A2_NON-VPN.py
@@ -15,30 +15,38 @@ KNN_accuracy = []
 DT_accuracy = []
 
 while time <= 120:
+  #upload dataset
   df = pd.read_csv(f'TimeBasedFeatures-Dataset-{time}s-NO-VPN.csv')
 
+  #divide independent and dependent columns
   x = df.iloc[:, :-1].values
   y = df.iloc[:, -1].values
 
+  #handle missing values
   imputer = SimpleImputer(missing_values=np.nan, strategy='mean')
   imputer.fit(x[:, :])
   x[:, :] = imputer.transform(x[:, :])
 
+  #handle categorical variable
   le = LabelEncoder()
   y = le.fit_transform(y)
 
+  #diviide into training set and test set
   x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.25, random_state = 1)
 
+  #feature scaling
   sc = StandardScaler()
   x_train = sc.fit_transform(x_train)
   x_test = sc.transform(x_test)
 
+  #model building
   classifier1 = KNeighborsClassifier(n_neighbors = 5, metric = 'minkowski', p = 2)
   classifier1.fit(x_train, y_train)
 
   classifier2 = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
   classifier2.fit(x_train, y_train) 
 
+  #prediction using model
   y_pred1 = classifier1.predict(x_test)
   y_pred2 = classifier2.predict(x_test)
 
